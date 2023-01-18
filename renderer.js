c3c0da56e299bdbb4bb803c061182c0045abc2f2
多厘 <zhaody901@126.com>
2023-01-19 02:06:52 +0800

    add version main world variables

--- renderer.js
+++ renderer.js
@@ -5,3 +5,7 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+const info = document.getElementById('info')
+info.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`
+
+
